6aff496186139b32e07816f0c5db53cb8a634ae5
wadeTechKnowPros <164968242+wadeTechKnowPros@users.noreply.github.com>
2024-04-14 21:32:06 -0400

    test

---
+++ SetTaskBarLayout.ps1
@@ -0,0 +1,47 @@
+###
+# Author: Dave Long <dlong@cagedata.com>
+# Last Udpated: 2021-07-21
+# Description: Sets the default taskbar for users to include File Explorer, MS Edge, Outlook and Teams
+###
+
+$TaskbarLayout = @'
+<?xml version="1.0" encoding="utf-8"?>
+<LayoutModificationTemplate
+    xmlns="http://schemas.microsoft.com/Start/2014/LayoutModification"
+    xmlns:defaultlayout="http://schemas.microsoft.com/Start/2014/FullDefaultLayout"
+    xmlns:start="http://schemas.microsoft.com/Start/2014/StartLayout"
+    xmlns:taskbar="http://schemas.microsoft.com/Start/2014/TaskbarLayout"
+    Version="1">
+  <CustomTaskbarLayoutCollection PinListPlacement="Replace">
+    <defaultlayout:TaskbarLayout>
+      <taskbar:TaskbarPinList>
+
+	<taskbar:DesktopApp DesktopApplicationID="Microsoft.Windows.Explorer" />
+	<taskbar:DesktopApp DesktopApplicationID="Chrome" />
+	<taskbar:DesktopApp DesktopApplicationID="Microsoft.Office.OUTLOOK.EXE.15" />
+	<taskbar:UWA AppUserModelID="MSTeams_8wekyb3d8bbwe!MSTeams" />
+	<taskbar:DesktopApp DesktopApplicationID="Microsoft.Office.WinWord.EXE.15" />
+	<taskbar:DesktopApp DesktopApplicationID="Microsoft.Office.Excel.EXE.15" />
+
+      </taskbar:TaskbarPinList>
+    </defaultlayout:TaskbarLayout>
+ </CustomTaskbarLayoutCollection>
+</LayoutModificationTemplate>
+'@
+If(-Not(Test-Path C:\Scripts )){
+  New-Item -Path C:\Scripts  -ItemType Directory -ErrorAction Stop | Out-Null
+}
+Set-Content -Path "C:\Scripts\DefaultTaskbarLayout.xml" -Value $TaskbarLayout
+
+$RegPath = "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer"
+if((Test-Path -LiteralPath "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer") -ne $true) {  New-Item "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer" -force -ea SilentlyContinue };
+New-Item -Path $RegPath
+New-ItemProperty -Path $RegPath -Name "StartLayoutFile" -PropertyType ExpandString -Value "C:\Scripts\DefaultTaskbarLayout.xml"
+New-ItemProperty -Path $RegPath -Name "LockedStartLayout" -PropertyType DWord -Value "1"
+
+taskkill /f /im explorer.exe
+
+start explorer.exe
+
+Remove-ItemProperty -path "HKCU:\software\policies\Microsoft\windows\explorer" -name startlayoutfile
+Remove-ItemProperty -path "HKCU:\software\policies\Microsoft\windows\explorer" -name lockedstartlayout
