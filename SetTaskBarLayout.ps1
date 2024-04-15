7bd680334b221acb3d244fb75ad8ad74b08c7900
wadeTechKnowPros <164968242+wadeTechKnowPros@users.noreply.github.com>
2024-04-14 21:52:18 -0400

    Update SetTaskBarLayout.ps1

--- SetTaskBarLayout.ps1
+++ SetTaskBarLayout.ps1
@@ -4,6 +4,15 @@
 # Description: Sets the default taskbar for users to include File Explorer, MS Edge, Outlook and Teams
 ###
 
+# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+ if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+  $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+  Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+  Exit
+ }
+}
+
 $TaskbarLayout = @'
 <?xml version="1.0" encoding="utf-8"?>
 <LayoutModificationTemplate
